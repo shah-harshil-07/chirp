3bd40cb861507dcaf6012cfc0f440c1931054abc
Harshil Shah <harshil.shah@bytestechnolab.com>
2023-03-21 06:40:47 +0530

    [Client] Worked on User Registeration Design. [WIP]

--- client/src/components/AuthBar/index.js
+++ client/src/components/AuthBar/index.js
@@ -5,14 +5,14 @@ import React from "react";
 const AuthBar = () => {
     return (
         <div id="auth-bar" className="row">
-            <div style={{ marginLeft: "35%" }}>
+            <div style={{ marginLeft: "32%" }}>
                 <div style={{ fontWeight: "bolder", fontSize: "20px", color: "white" }}>Don't miss what's happening</div><br />
                 <div style={{ fontSize: "13px", color: "white", marginTop: "-28px" }}>People on Chirp are first to know</div>
             </div>
 
             <div style={{ marginRight: "14%", marginTop: "-35px" }}>
-                <div className="auth-btn" id="signup-btn"><span style={{ fontSize: "11px" }}>Sign Up</span></div>
-                <div className="auth-btn" id="login-btn"><span style={{ fontSize: "11px" }}>Log in</span></div>
+                <div className="auth-btn" id="signup-btn"><span style={{ fontSize: "13px" }}>Sign Up</span></div>
+                <div className="auth-btn" id="login-btn"><span style={{ fontSize: "13px" }}>Log in</span></div>
             </div>
         </div>
     )
