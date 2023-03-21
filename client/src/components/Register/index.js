1ea810164f6ba379746ef54e9e6e30c40a165320
Harshil Shah <harshil.shah@bytestechnolab.com>
2023-03-21 06:40:47 +0530

    [Client] Worked on User Registeration. - [DONE]

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -2,12 +2,16 @@ import "./index.css";
 import React from "react";
 
 const Register = () => {
+    const logo = require("../../assets/logo-1.png");
+
     return (
         <div className="custom-modal">
             <div className="custom-modal-content">
                 <header className="custom-container">
                     <div className="row custom-header-box">
-                        <div className="col-sm-11"><h3>Delete User</h3></div>
+                        <div className="col-sm-11">
+                            <center><img alt="logo" width="40px" height="40px" src={String(logo)} /></center>
+                        </div>
 
                         <div className="col-sm-1 custom-close-div">
                             <span className="custom-close-btn">&times;</span>
@@ -16,7 +20,9 @@ const Register = () => {
                 </header>
 
                 <div className="custom-container-body">
-                    <p>Are you Sure you want to delete?</p>
+                    <div style={{ width: "100%", height: "30px", border: "1px solid black", borderRadius: "20px", paddingLeft: "12px" }}>
+                        Sign up with google
+                    </div>
                 </div>
 
                 <footer className="custom-modal-footer">
