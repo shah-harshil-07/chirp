f2d25e4fb0c8aa9b6fd201822d62d743ed7b44c0
Harshil Shah <harshil.shah@brainvire.com>
2023-04-18 11:44:48 +0530

    [Server] Formatted 404 and 500 responses. - [DONE]

--- server/src/interceptors/response.ts
+++ server/src/interceptors/response.ts
@@ -1,4 +1,4 @@
-import { NestInterceptor, ExecutionContext, CallHandler, Injectable, HttpException, HttpStatus } from "@nestjs/common";
+import { NestInterceptor, ExecutionContext, CallHandler, Injectable, InternalServerErrorException } from "@nestjs/common";
 
 import { Observable } from "rxjs";
 import { catchError, map } from "rxjs/operators";
@@ -23,30 +23,19 @@ interface IErrorResponse {
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
     private successResponse(data: any, message: string): ISuccessResponse {
-        return {
-            meta: { message, messageCode: "SUCCESS", status: true, statusCode: 200 },
-            data,
-        };
+        return { meta: { message, messageCode: "SUCCESS", status: true, statusCode: 200 }, data };
     }
 
     private errorResponse(error: any, statusCode: number): IErrorResponse {
-        return {
-            meta: { statusCode, message: "Error", messageCode: "ERROR", status: false },
-            error
-        };
+        const message = statusCode === 400 ? "Bad Request" : "Error";
+        return { meta: { statusCode, message, messageCode: "ERROR", status: false }, error };
     }
 
     public intercept(_: ExecutionContext, next: CallHandler<any>): Observable<ISuccessResponse | IErrorResponse> {
         return next.handle().pipe(
             map(data => this.successResponse(data.data, data.message)),
             catchError(async err => {
-                if (err?.status === 500) {
-                    throw new HttpException(
-                        "INTERNAL_SERVER_ERROR",
-                        HttpStatus.INTERNAL_SERVER_ERROR,
-                        { cause: new Error(), description: "Server Error" },
-                    );
-                }
+                if (err?.status === 500) throw new InternalServerErrorException();
 
                 const errorObj = { message: err?.response?.message?.[0] ?? "Validation Error" };
                 const statusCode = err?.status ?? 422;
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { HttpExceptionFilter } from "./exception-handlers/http-exception";
+import { InternalServerExceptionFilter } from "./exception-handlers/500";
+import { NotFoundExceptionFilter } from "./exception-handlers/404";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	app.useGlobalPipes(new ValidationPipe());
-	app.useGlobalFilters(new HttpExceptionFilter());
+	app.useGlobalFilters(new InternalServerExceptionFilter(), new NotFoundExceptionFilter());
 	await app.listen(process.env.PORT);
 }
 
