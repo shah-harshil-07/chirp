fb12f040de8314133fae91fae1386f8291e88116
Harshil Shah <harshil.shah@bytestechnolab.com>
2023-04-26 06:45:31 +0530

    [Client] Worked on design for uploading emojis and creating a poll. - [DONE]

--- client/src/components/form/poll-creator.js
+++ client/src/components/form/poll-creator.js
@@ -30,67 +30,66 @@ const PollCreator = () => {
 
     return (
         <div id="poll-creator-box">
-            {
-                choices.map((choice, choiceIndex) => {
-                    const n = choices.length;
+            <div style={{ borderBottom: "1px solid rgba(29, 161, 242, 0.6)", paddingLeft: "10px" }}>
+                {
+                    choices.map((choice, choiceIndex) => {
+                        const n = choices.length;
 
-                    return (
-                        <div style={{ display: "flex" }} key={choiceIndex}>
-                            <LabelledInput
-                                value={choice}
-                                extraClasses={"choice-input-box"}
-                                header={`Choice ${choiceIndex + 1}`}
-                                handleChange={data => handleChoiceInput(data, choiceIndex)}
-                            />
+                        return (
+                            <div style={{ display: "flex" }} key={choiceIndex}>
+                                <LabelledInput
+                                    value={choice}
+                                    extraClasses={"choice-input-box"}
+                                    header={`Choice ${choiceIndex + 1}`}
+                                    handleChange={data => handleChoiceInput(data, choiceIndex)}
+                                />
 
-                            {
-                                choiceIndex === n - 1 && choiceIndex < 3 && (
-                                    <div className="add-choice-icon" onClick={addChoice}>
-                                        <CIcon icon={cilPlus} size="sm" />
-                                    </div>
-                                )
-                            }
-                        </div>
-                    )
-                })
-            }
+                                {
+                                    choiceIndex === n - 1 && choiceIndex < 3 && (
+                                        <div className="add-choice-icon" onClick={addChoice}>
+                                            <CIcon icon={cilPlus} size="sm" />
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
-            <hr />
+            <div style={{ paddingLeft: "10px", paddingTop: "10px", paddingBottom: "10px", borderBottom: "1px solid rgba(29, 161, 242, 0.6)" }}>
+                <h6><b>Poll Length</b></h6>
 
-            <h6><b>Poll Length</b></h6>
-            <div className="row w-100 ml-0 mb-3">
-                <div style={{ width: "33%", paddingLeft: "5px", paddingRight: "5px" }}>
-                    <label>Days</label>
-                    <select className="w-100">
-                        {
-                            dayOptions.map(day => (
-                                <option value={day}>{day}</option>
-                            ))
-                        }
-                    </select>
+                <div className="row w-100 ml-0">
+                    <div style={{ width: "33%", paddingLeft: "5px", paddingRight: "5px", position: "relative" }}>
+                        <label style={{ position: "absolute", left: "10px" }}>Days</label>
+
+                        <select className="w-100" style={{ height: "50px", paddingTop: "20px", paddingLeft: "1px", border: "1px solid rgba(29, 161, 242, 0.6)", backgroundColor: "white", cursor: "pointer" }}>
+                            {dayOptions.map(day => (<option value={day}>{day}</option>))}
+                        </select>
+                    </div>
+
+                    <div style={{ width: "33%", paddingLeft: "5px", paddingRight: "5px", position: "relative" }}>
+                        <label style={{ position: "absolute", left: "10px" }}>Hours</label>
+
+                        <select className="w-100" style={{ height: "50px", paddingTop: "20px", paddingLeft: "1px", border: "1px solid rgba(29, 161, 242, 0.6)", backgroundColor: "white", cursor: "pointer" }}>
+                            {hourOptions.map(hour => (<option value={hour}>{hour}</option>))}
+                        </select>
+                    </div>
+
+                    <div style={{ width: "28%", paddingLeft: "5px", paddingRight: "5px", position: "relative" }}>
+                        <label style={{ position: "absolute", left: "10px" }}>Minutes</label>
+
+                        <select className="w-100" style={{ height: "50px", paddingTop: "20px", paddingLeft: "1px", border: "1px solid rgba(29, 161, 242, 0.6)", backgroundColor: "white", cursor: "pointer" }}>
+                            {minuteOptions.map(minute => (<option value={minute}>{minute}</option>))}
+                        </select>
+                    </div>
                 </div>
+            </div>
 
-                <div style={{ width: "33%", paddingLeft: "5px", paddingRight: "5px" }}>
-                    <label>Hours</label>
-                    <select className="w-100">
-                        {
-                            hourOptions.map(hour => (
-                                <option value={hour}>{hour}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-
-                <div style={{ width: "28%", paddingLeft: "5px", paddingRight: "5px" }}>
-                    <label>Minutes</label>
-                    <select className="w-100">
-                        {
-                            minuteOptions.map(minute => (
-                                <option value={minute}>{minute}</option>
-                            ))
-                        }
-                    </select>
-                </div>
+            <div style={{ display: "flex", height: "50px" }}>
+                <div style={{ textAlign: "center", width: "100%", paddingTop: "14px", borderRight: "1px solid rgba(29, 161, 242, 0.6)" }}>Reset Poll</div>
+                <div style={{ textAlign: "center", width: "100%", paddingTop: "14px" }}>Remove Poll</div>
             </div>
         </div>
     )
