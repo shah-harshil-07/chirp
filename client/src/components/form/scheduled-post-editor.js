db1736ad0ecd58123bf184fceb94d44c1898721d
Harshil Shah <harshil.shah@brainvire.com>
2023-05-24 19:31:41 +0530

    [Client] Worked on generating file object from base64 image. - [WIP]

--- client/src/components/form/scheduled-post-editor.js
+++ client/src/components/form/scheduled-post-editor.js
@@ -6,8 +6,20 @@ import CustomModal from "../utilities/custom-modal";
 import Form from "./index";
 
 const ScheduledPostEditor = props => {
-    useEffect(() => {
+    useEffect(async () => {
         console.log("props obtained => ", props);
+        if (props?.images?.length) {
+            const image = props.images[0];
+            const blobImage = new Uint8ClampedArray(image);
+            const blob = new Blob([blobImage]);
+            const file = new File([blob], "sample-img.jpeg");
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = e => {
+                console.log(e.target.result);
+            };
+            console.log(file);
+        }
     }, [props]);
 
     return (
