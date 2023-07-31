19674f95be3e0519ec5f4d2fd54637b9db7b98b5
Harshil Shah <harshil.shah@bytestechnolab.com>
2023-07-31 07:38:13 +0530

    [Client] Worked on user actionbar in the left sidebar's design and features. - [DONE]

--- client/src/components/sidebar/left-sidebar.jsx
+++ client/src/components/sidebar/left-sidebar.jsx
@@ -1,17 +1,39 @@
 import "src/styles/sidebar/index.css";
 import "src/styles/sidebar/left-sidebar.css";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import CIcon from "@coreui/icons-react";
 import { cilHome, cilSettings, cilBookmark, cilUser, cilOptions } from "@coreui/icons";
 
 import { getUserDetails, isUserLoggedIn } from "src/utilities/helpers";
+import Confirmation from "../utilities/confirmation";
 
 const LeftSidebar = () => {
     const logo = require("src/assets/logo-1.png");
     const placeHolderImageSrc = "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png";
 
-    const userDetails = getUserDetails();
+    const userDetails = getUserDetails(), actionbarRef = useRef(null), actionIconRef = useRef(null);
+
+    const [viewActionbar, setViewActionbar] = useState(false);
+    const [openLogoutConfirmation, setOpenLogoutConfirmation] = useState(false);
+
+    document.addEventListener("click", e => {
+        e.stopImmediatePropagation();
+
+        if (
+            !actionbarRef?.current?.contains(e.target) &&
+            !actionIconRef?.current?.contains(e.target)
+        ) {
+            setViewActionbar(false);
+        }
+    });
+
+    const logoutUser = () => {
+        localStorage.removeItem("chirp-accessToken");
+        localStorage.removeItem("chirp-userDetails");
+        setOpenLogoutConfirmation(false);
+        window.location.reload();
+    }
 
     return (
         <div className="sidebar">
@@ -62,23 +84,51 @@ const LeftSidebar = () => {
 
                 <div id="left-sidebar-chirp-btn">Post</div>
 
-                {
-                    isUserLoggedIn() && userDetails?.name && userDetails?.username && (
-                        <div className="row" id="left-sidebar-profile-container">
-                            <div id="profile-image-container">
-                                <img
-                                    alt="logo"
-                                    className="sidebar-profile-img"
-                                    src={userDetails.picture ? userDetails.picture : placeHolderImageSrc}
+                <div>
+                    {
+                        viewActionbar && (
+                            <div className="left-sidebar-user-actionbar" ref={actionbarRef}>
+                                <div className="left-sidebar-user-action" onClick={() => { setOpenLogoutConfirmation(true); }}>
+                                    <b>Log out {`@${userDetails?.username ?? ''}`}</b>
+                                </div>
+                            </div>
+                        )
+                    }
+                    {
+                        isUserLoggedIn() && userDetails?.name && userDetails?.username && (
+                            <div className="row" id="left-sidebar-profile-container">
+                                <div id="profile-image-container">
+                                    <img
+                                        alt="logo"
+                                        className="sidebar-profile-img"
+                                        src={userDetails.picture ? userDetails.picture : placeHolderImageSrc}
+                                    />
+                                </div>
+
+                                <div style={{ fontSize: "19px" }}>
+                                    &nbsp;{userDetails?.name ?? ''}<br />{`@${userDetails?.username ?? ''}`}
+                                </div>
+
+                                <CIcon
+                                    icon={cilOptions}
+                                    ref={actionIconRef}
+                                    className="options-icon"
+                                    style={{ marginLeft: "21px" }}
+                                    onClick={() => { setViewActionbar(!viewActionbar); }}
                                 />
                             </div>
+                        )
+                    }
+                </div>
 
-                            <div style={{ fontSize: "19px" }}>
-                                &nbsp;{userDetails?.name ?? ''}<br />{`@${userDetails?.username ?? ''}`}
-                            </div>
-
-                            <CIcon className="options-icon" style={{ marginLeft: "21px" }} icon={cilOptions} />
-                        </div>
+                {
+                    openLogoutConfirmation && (
+                        <Confirmation
+                            headingText={"Logout"}
+                            handleConfirmAction={logoutUser}
+                            message={`Are you sure you want to logout?`}
+                            handleCloseAction={() => { setOpenLogoutConfirmation(false); }}
+                        />
                     )
                 }
             </div>
