29cac52d0fb0cc297fc0d6692927f18f9736bee9
Harshil Shah <harshil.shah@brainvire.com>
2023-10-16 10:22:54 +0530

    [Client] Revamped modal redux with createSlice method. - [DONE]

--- client/src/redux/actions/modal.js
+++ client/src/redux/actions/modal.js
@@ -1,19 +1,16 @@
-function openModal(state, { payload }) {
-    state.open = true;
-    state.type = payload.type;
+export default {
+    openModal(state, { payload }) {
+        state.open = true;
+        state.type = payload.type;
+    },
+    closeModal(state) {
+        state.open = false;
+        state.type = '';
+    },
+    openModalWithProps(state, { payload }) {
+        state.open = true;
+        const { props, type } = payload;
+        state.type = type;
+        state.props = props;
+    },
 };
-
-function closeModal(state) {
-    state.open = false;
-    state.type = '';
-};
-
-function openModalWithProps(state, { payload }) {
-    state.open = true;
-    const { props, type } = payload;
-    state.type = type;
-    state.props = props;
-};
-
-const actions = { openModal, closeModal, openModalWithProps };
-export default actions;
--- client/src/redux/reducers/modal.js
+++ client/src/redux/reducers/modal.js
@@ -1,30 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const modalSlice = createSlice({
-    name: "modals",
-    initialState: {
-        modal: false,
-        open: false,
-        type: '',
-        props: {},
-    },
-    reducers: {
-        openModal(state, { payload }) {
-            state.open = true;
-            state.type = payload.type;
-        },
-        closeModal(state) {
-            state.open = false;
-            state.type = '';
-        },
-        openModalWithProps(state, { payload }) {
-            state.open = true;
-            const { props, type } = payload;
-            state.type = type;
-            state.props = props;
-        },
-    },
-});
+import modelActions from "src/redux/actions/modal";
+
+const name = "modals";
+const reducers = { ...modelActions };
+const initialState = { modal: false, open: false, type: '', props: {} };
+
+const modalSlice = createSlice({ name, initialState, reducers });
 
 const { actions, reducer } = modalSlice;
 export const { openModal, closeModal, openModalWithProps } = actions;
