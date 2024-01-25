37beef5a9f2ee1fdaac846075886dcf44d104d61
Harshil Shah <shahharshil1998@gmail.com>
2024-01-25 06:31:47 +0530

    [Client] Worked on lighthouse design. - [DONE]

--- client/src/components/utilities/lighthouse.jsx
+++ client/src/components/utilities/lighthouse.jsx
@@ -1,14 +1,39 @@
 import "src/styles/utilities/lighthouse.css";
 
+import CIcon from "@coreui/icons-react";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { cilArrowLeft, cilArrowRight } from "@coreui/icons";
+
+import { closeLighthouse } from "src/redux/reducers/lighthouse";
 
 const LightHouse = ({ images, initialIndex }) => {
+    const dispatch = useDispatch();
+    const bodyDiv = document.querySelector("body");
+
     useEffect(() => {
-        document.querySelector("body").style.overflow = "hidden";
+        bodyDiv.style.overflow = "hidden";
+        return () => { bodyDiv.style.overflow = "auto"; };
     }, []);
+
+    const closeZoom = () => {
+        dispatch(closeLighthouse());
+    }
 
     return (
         <div className="lighthouse-container" style={{ top: window.scrollY }}>
+            <div className="lighthouse-close-btn" onClick={closeZoom}>
+                <span className="lighthouse-close-icon">&times;</span>
+            </div>
+
+            <div className="lighthouse-arrow-container" id="lighthouse-adjust-left">
+                <CIcon icon={cilArrowLeft} />
+            </div>
+
+            <div className="lighthouse-arrow-container" id="lighthouse-adjust-right">
+                <CIcon icon={cilArrowRight} />
+            </div>
+
             <img alt="zoomed canva" src={images[initialIndex]} className="zoomed-image" />
         </div>
     );
